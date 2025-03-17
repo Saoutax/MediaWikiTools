9b2b5554efca8b2c34a64220d6005ae4dfde1fba
亚衣琴 <193034419+SAOUTAX@users.noreply.github.com>
2025-03-17 12:24:55 +0800

    Create DraftToMain.js

---
+++ DraftToMain.js
@@ -0,0 +1,33 @@
+if (mw.config.get("wgNamespaceNumber") === 2) { 
+    $(mw.util.addPortletLink("p-cactions", "#", "移动到主名字空间", "move-to-main", "移动到主名字空间", "m")).on("click", function () {
+        var userPageTitle = mw.config.get("wgPageName");
+        var slashIndex = userPageTitle.indexOf("/");
+
+        if (slashIndex === -1) {
+            mw.notify("标题获取失败");
+            return;
+        }
+
+        var newTitle = userPageTitle.substring(slashIndex + 1);
+
+        
+        new mw.Api().postWithToken("csrf", {
+            action: "move",
+            from: userPageTitle,
+            to: newTitle,
+            reason: "编写完成",
+            movetalk: "noleave",
+            noredirect: true,
+            format: "json"
+        })
+        .done(function () {
+            mw.notify("页面已成功移动到主名字空间，即将跳转……");
+            setTimeout(function () {
+                window.location.href = mw.util.getUrl(newTitle);
+            }, 2000);
+        })
+        .fail(function (err) {
+                mw.notify("移动时出现错误：" + err + "。");
+            })
+    });
+}
