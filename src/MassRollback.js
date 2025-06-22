f5e80956225c996a726c73868627d917da8ddcaf
亚衣琴 <Saoutax@gmail.com>
2025-06-22 15:42:30 +0800

    style:

--- src/MassRollback.js
+++ src/MassRollback.js
@@ -42,7 +42,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
 
     $("#contributions-rollback-button").click(async () => {
         const checked = $(".mw-contributions-list li :checkbox:checked");
-        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面</li><li>批量回退操作的编辑摘要：<code>xxx//MassRollback</code></li><li>空白则使用默认回退摘要，取消则不进行回退</li><li>管理员可自授权机器用户或在URL后添加<code>bot=1</code>以启用markbotedit。</li></ul><hr>请输入回退摘要：`, {
+        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面</li><li>批量回退操作的编辑摘要：<code>xxx// MassRollback</code></li><li>空白则使用默认回退摘要，取消则不进行回退</li><li>管理员可自授权机器用户或在URL后添加<code>bot=1</code>以启用markbotedit。</li></ul><hr>请输入回退摘要：`, {
             title: "批量回退小工具",
             size: "medium",
             required: false,
@@ -61,7 +61,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
                     markbot: mw.config.get("wgUserGroups").includes("sysop") && (mw.config.get("wgUserGroups").includes("flood") || document.URL.includes("bot=1")),
                     watchlist: "nochange",
                     tags: "Automation tool",
-                    summary: reason ? `${reason} //MassRollback` : "//MassRollback",
+                    summary: reason ? `${reason} // MassRollback` : "// MassRollback",
                 }).then((result) => {
                     console.log(`回退：${title}\n${result}`);
                 });
@@ -73,7 +73,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
 
     $("#contributions-undo-button").click(async () => {
         const checked = $(".mw-contributions-list li :checkbox:checked");
-        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面</li><li>批量撤销操作的编辑摘要：<code>xxx//MassUndo</code></li><li>空白则使用默认撤销摘要，取消则不进行撤销</li></ul><hr>请输入撤销摘要：`, {
+        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面</li><li>批量撤销操作的编辑摘要：<code>xxx// MassUndo</code></li><li>空白则使用默认撤销摘要，取消则不进行撤销</li></ul><hr>请输入撤销摘要：`, {
             title: "批量撤销小工具",
             size: "medium",
             required: false,
@@ -92,7 +92,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
                     tags: "Automation tool",
                     bot: mw.config.get("wgUserGroups").includes("flood"),
                     watchlist: "nochange",
-                    summary: reason ? `${reason} //MassUndo` : "//MassUndo",
+                    summary: reason ? `${reason} // MassUndo` : "// MassUndo",
                 }).then((result) => {
                     console.log(`撤销：${title}\n${result}`);
                 });
@@ -105,7 +105,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
     $("#contributions-registerdelete-button").click(async () => {
         const checked = $(".mw-contributions-list li :checkbox:checked");
         const username = mw.config.get("wgUserName");
-        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面</li><li>批量挂删操作的编辑摘要：<code>xxx//MassDelete</code></li><li>空白则使用默认摘要，取消则不进行挂删</li></ul><hr>请输入挂删摘要：`, {
+        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面</li><li>批量挂删操作的编辑摘要：<code>xxx// MassDelete</code></li><li>空白则使用默认摘要，取消则不进行挂删</li></ul><hr>请输入挂删摘要：`, {
             title: "批量挂删小工具",
             size: "medium",
             required: false,
@@ -123,7 +123,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
                     tags: "Automation tool",
                     bot: mw.config.get("wgUserGroups").includes("flood"),
                     watchlist: "nochange",
-                    summary: reason ? `${reason} //MassDelete` : "//MassDelete",
+                    summary: reason ? `${reason} // MassDelete` : "// MassDelete",
                 }).then((result) => {
                     console.log(`挂删：${title}\n${result}`);
                 });
@@ -135,7 +135,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
 
     $("#contributions-revdel-button").click(async () => {
         const checked = $(".mw-contributions-list li :checkbox:checked");
-        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面，将删除版本内容和编辑摘要</li><li>批量版本删除的原因：<code>xxx//MassRevisionDelete</code></li><li>空白则使用默认原因，取消则不进行版本删除</li></ul><hr>请输入版本删除原因：`, {
+        const reason = await oouiDialog.prompt(`<ul><li>选中了${checked.length}个页面，将删除版本内容和编辑摘要</li><li>批量版本删除的原因：<code>xxx// MassRevisionDelete</code></li><li>空白则使用默认原因，取消则不进行版本删除</li></ul><hr>请输入版本删除原因：`, {
             title: "批量版本删除小工具",
             size: "medium",
             required: false,
@@ -154,7 +154,7 @@ $.when($.ready, mw.loader.using(["mediawiki.api", "ext.gadget.libOOUIDialog"])).
                     ids: revid,
                     tags: "Automation tool",
                     hide: "comment|content",
-                    reason: reason ? `${reason} //MassRevisionDelete` : "//MassRevisionDelete",
+                    reason: reason ? `${reason} // MassRevisionDelete` : "// MassRevisionDelete",
                 }).then((result) => {
                     console.log(`版本删除：${title}\n${result}`);
                 });
